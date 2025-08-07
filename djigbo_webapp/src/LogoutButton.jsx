ca5e54503cd3c7eea363f93f8bd596b37269e69f
somkamarius <marius.somka@mif.stud.vu.lt>
2025-08-07 09:20:15 +0300

    typo profilis

--- djigbo_webapp/src/LogoutButton.jsx
+++ djigbo_webapp/src/LogoutButton.jsx
@@ -16,7 +16,7 @@ const LogoutButton = () => {
                         <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
                     </svg>
                 </div>
-                <span className="logout-button-title">Profile</span>
+                <span className="logout-button-title">Profilis</span>
                 <div className="logout-button-indicator">→</div>
             </div>
         </button>
