4ab9719647ba2666467909cdb21b42fdea3e9ecb
somkamarius <marius.somka@mif.stud.vu.lt>
2025-08-07 09:13:50 +0300

    typos

--- djigbo_webapp/src/ClimateNavigationButton.jsx
+++ djigbo_webapp/src/ClimateNavigationButton.jsx
@@ -46,7 +46,7 @@ const ClimateNavigationButton = () => {
                             <path d="M20 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h4l4 4 4-4h4c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" />
                         </svg>
                     </div>
-                    <span className="climate-button-title">Chat</span>
+                    <span className="climate-button-title">Chat'as</span>
                     <div className="climate-button-indicator">→</div>
                 </div>
             </Link>
